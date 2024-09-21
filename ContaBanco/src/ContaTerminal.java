b96fbd216777dde333424faabf6fff5e6b43874a
factimao <factimao@gmal.com>
2024-09-21 11:22:21 -0300

    commit

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,16 +1,28 @@
+import java.util.Locale;
 import java.util.Scanner;
 
 public class ContaTerminal {
     
     public static void main(String[] args) throws Exception {
 
-        Scanner scanner = new Scanner(null)
-        int numero = 1021;
-        String agencia = "067-8";
-        String nomeCliente = "MARIO ANDRADE";
-        double saldo = 234.48;
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        String nomeCliente;
+        String agencia;
+        int numeroConta;
+        double saldo;
 
+        System.out.println("Digite nome:");
+        nomeCliente = scanner.next();
+        System.out.println("Digite agência:");
+        agencia = scanner.next();
+        System.out.println("Digite número da conta:");
+        numeroConta = scanner.nextInt();
+        System.out.println("Digite saldo:");
+        saldo = scanner.nextDouble();
 
-        System.out.println("Hello, World!");
+        System.out.println("Olá " + nomeCliente + 
+        ", obrigado por criar uma conta em nosso banco, sua agência é " + agencia + 
+        ", conta " + numeroConta + " e seu saldo " + saldo + " já está disponível para saque.");
+
     }
 }
