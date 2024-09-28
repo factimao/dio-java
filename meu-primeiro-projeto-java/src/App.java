52da21758f5324ea145d64b5946dd25b7fe35ebd
factimao <factimao@gmal.com>
2024-09-28 10:48:04 -0300

    controle-candidatos commit inicial

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,27 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("testes testse tetse");
+    public static void main(String[] args) {
+
+        String cep = "12";
+
+        try {
+            formatarCep(cep);
+        } catch (CepInvalidoException e) {
+            // TODO fazer alguma coisa
+            e.printStackTrace();
+            System.err.println("ERRO ERRO ERRO");
+        }
+
+
+
     }
+
+    static String formatarCep(String cep) throws CepInvalidoException{
+        if (cep.length() < 8){
+            throw new CepInvalidoException();
+        };
+        return "12345678";
+        
+    }
+
+
 }
